de0bcf2603062ab4d0ede0177b268b4c5827ab64
kishanYuvmedia <142478981+kishanYuvmedia@users.noreply.github.com>
2026-09-08 17:00:21 +0530

    javascript

---
+++ javascript/file.js
@@ -0,0 +1,23 @@
+  let json = {
+            name: 'Rajesh',
+            age: 24,
+            city: 'Ajmer',
+            hobbies: ['Music', 'Dance', "Read Book"],
+            shippingAddress: {
+                OfficeAddress: {
+                    currentAddress: "Vaishali Nagar Ajmer",
+                    pin: "302003",
+                    city: 'Ajmer',
+                    ladmark: 'shiv temple',
+                    state: 'Rajasthan'
+                },
+                HomeAdress: {
+                    currentAddress: "Vaishali Nagar Ajmer",
+                    pin: "302003",
+                    city: 'Ajmer',
+                    ladmark: 'shiv temple',
+                    state: 'Rajasthan'
+                }
+            }
+        }
+      
---
+++ javascript/funcaiton.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+    return a + b;
+}
+let s=sum(5, 10); // returns 15
+
+function sum() {
+    return 10+12;
+}
+let v=sum();
+
+function print() {
+    console.log("Hello");
+}
+print();
